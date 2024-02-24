81fbd1bda20f8572ea56886b4c672d2f09adc6d6
Daniël de Kok <me@danieldk.eu>
2024-02-24 13:25:32 +0100

    Add keymap

---
+++ keyboards/voyager/keymaps/danieldk/config.h
@@ -0,0 +1,16 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+
+// Enabled through ACHORDION_ENABLE.
+// #define PERMISSIVE_HOLD
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"yG3bz/b5gXx"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
